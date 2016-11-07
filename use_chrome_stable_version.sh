0ad83a6064fd9ca4acbeb505a8c9d4c830805e6a
jeremypele <jeremy.pele@gmail.com>
2016-11-07 12:07:44 -0500

    Add stable Chrome version script

---
+++ use_chrome_stable_version.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+function use_chrome_stable_version {
+  curl -L -o google-chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb;
+  sudo dpkg -i google-chrome.deb;
+  sudo sed -i 's|HERE/chrome\"|HERE/chrome\" --disable-setuid-sandbox|g' /opt/google/chrome/google-chrome;
+  rm google-chrome.deb
+}
+
+use_chrome_stable_version
